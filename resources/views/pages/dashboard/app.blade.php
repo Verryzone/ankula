5393237d7d7299bdcfb9fd8542feefc3a6b90193
Verryzone <umu369852147@gmail.com>
2025-07-10 22:06:41 +0700

    fix tampilan dashboard

--- resources/views/pages/dashboard/app.blade.php
+++ resources/views/pages/dashboard/app.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
-    <div class="max-w-screen-xl mx-auto flex min-h-screen">
-        <div class="px-4 py-2">
-            <div class="w-full">
-                <div class="space-y-12 py-8">
+    <div class="max-w-screen-xl mx-auto p-4">
+        <div class="space-y-12 py-8">
 
                     <section class="bg-blue-800 text-white rounded-lg overflow-hidden">
                         <div class="flex flex-col md:flex-row items-center">
@@ -113,8 +111,6 @@
                     </section>
                 </div>
             </div>
-        </div>
-    </div>
 
     <!-- Product Detail Modal -->
     <div id="productModal" class="fixed inset-0 bg-black bg-opacity-50 z-50 hidden items-center justify-center p-4">
